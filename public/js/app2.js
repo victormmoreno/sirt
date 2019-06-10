27f2410e158ec166db827982d1a3cb2d9ea3c8dc
Victor Manuel Moreno Vega <vmmv99@hotmail.com>
2019-06-10 08:05:21 -0500

    Cambios en gitignore

--- public/js/app2.js
+++ public/js/app2.js
@@ -127,114 +127,114 @@ $(document).ready(function() {
             
 
 });
-$(document).ready(function() {
-  $('#ideas_emprendedores_table').DataTable({
-    language: {
-      "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
-    },
-    processing: true,
-    serverSide: true,
-    ajax:{
-      url: "idea",
-      type: "get",
-    },
-    columns: [
-      {
-        data: 'consecutivo',
-        name: 'consecutivo',
-      },
-      {
-        data: 'fecha_registro',
-        name: 'fecha_registro',
-      },
-      {
-        data: 'persona',
-        name: 'persona',
-      },
-      {
-        data: 'correo',
-        name: 'correo',
-      },
-      {
-        data: 'contacto',
-        name: 'contacto',
-      },
-      {
-        data: 'nombre_idea',
-        name: 'nombre_idea',
-      },
-      {
-        data: 'estado',
-        name: 'estado',
-      },
-      {
-        data: 'details',
-        name: 'details',
-        orderable: false
-      },
-      {
-        data: 'edit',
-        name: 'edit',
-        orderable: false
-      },
-      {
-        data: 'soft_delete',
-        name: 'soft_delete',
-        orderable: false
-      },
-      {
-        data: 'dont_apply',
-        name: 'dont_apply',
-        orderable: false
-      },
-
-    ],
-  });
-
-  $('#ideas_emprendedores_table .dataTables_length select').addClass('browser-default');
-
-
-});
-
-// function secondDataTable() {
-//   if (!$.fn.dataTable.isDataTable('#tblideasempresas')) {
-//     $('#tblideasempresas').DataTable({
-//       language: {
-//         "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
+// $(document).ready(function() {
+//   $('#ideas_emprendedores_table').DataTable({
+//     language: {
+//       "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
+//     },
+//     processing: true,
+//     serverSide: true,
+//     ajax:{
+//       url: "idea",
+//       type: "get",
+//     },
+//     columns: [
+//       {
+//         data: 'consecutivo',
+//         name: 'consecutivo',
 //       },
-//       processing: true,
-//       serverSide: true,
-//       // order: [[0, 'desc']],
-//       // buttons: [
-//         //     'csv', 'excel', 'pdf', 'print', 'reset', 'reload'
-//         // ],
-//         ajax: 'idea/ideasEmpGI',
-//         columns: [
-//           {
-//             data: 'consecutivo',
-//             name: 'consecutivo',
-//           },
-//           {
-//             data: 'fecha_registro',
-//             name: 'fecha_registro',
-//           },
-//           {
-//             data: 'nit',
-//             name: 'nit',
-//           },
-//           {
-//             data: 'razon_social',
-//             name: 'razon_social',
-//           },
-//           {
-//             data: 'nombre_idea',
-//             name: 'nombre_idea',
-//           },
-//         ],
-//         // order: [[0, 'desc']]
-//       });
-//     }
-//   }
+//       {
+//         data: 'fecha_registro',
+//         name: 'fecha_registro',
+//       },
+//       {
+//         data: 'persona',
+//         name: 'persona',
+//       },
+//       {
+//         data: 'correo',
+//         name: 'correo',
+//       },
+//       {
+//         data: 'contacto',
+//         name: 'contacto',
+//       },
+//       {
+//         data: 'nombre_idea',
+//         name: 'nombre_idea',
+//       },
+//       {
+//         data: 'estado',
+//         name: 'estado',
+//       },
+//       {
+//         data: 'details',
+//         name: 'details',
+//         orderable: false
+//       },
+//       {
+//         data: 'edit',
+//         name: 'edit',
+//         orderable: false
+//       },
+//       {
+//         data: 'soft_delete',
+//         name: 'soft_delete',
+//         orderable: false
+//       },
+//       {
+//         data: 'dont_apply',
+//         name: 'dont_apply',
+//         orderable: false
+//       },
+//
+//     ],
+//   });
+//
+//   $('#ideas_emprendedores_table .dataTables_length select').addClass('browser-default');
+//
+//
+// });
+
+function secondDataTable() {
+  if (!$.fn.dataTable.isDataTable('#tblideasempresas')) {
+    $('#tblideasempresas').DataTable({
+      language: {
+        "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
+      },
+      processing: true,
+      serverSide: true,
+      // order: [[0, 'desc']],
+      // buttons: [
+        //     'csv', 'excel', 'pdf', 'print', 'reset', 'reload'
+        // ],
+        ajax: 'idea/ideasEmpGI',
+        columns: [
+          {
+            data: 'consecutivo',
+            name: 'consecutivo',
+          },
+          {
+            data: 'fecha_registro',
+            name: 'fecha_registro',
+          },
+          {
+            data: 'nit',
+            name: 'nit',
+          },
+          {
+            data: 'razon_social',
+            name: 'razon_social',
+          },
+          {
+            data: 'nombre_idea',
+            name: 'nombre_idea',
+          },
+        ],
+        // order: [[0, 'desc']]
+      });
+    }
+  }
 
 $('#ideas_emprendedores_table .dataTables_length select').addClass('browser-default');
 
