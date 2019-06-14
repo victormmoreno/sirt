01f36ceab9e6882f12edc75a4c6032eeaf44802f
Victor Manuel Moreno Vega <vmmv99@hotmail.com>
2019-06-14 16:43:59 -0500

    Se comenzó a trabajar en el módulo de entrenamientos y comités, se tienen ya hecha las consultas en sus respectivos roles

---
+++ resources/app/user/administrador/index-dinamizador.js
@@ -0,0 +1,47 @@
+var UserAdministradorDinamizador = {
+     selectDinamizadoresPorNodo: function() {
+         let nodo = $('#selectnodo').val();
+         $('#dinamizador_table').dataTable().fnDestroy();
+         $('#dinamizador_table').DataTable({
+             language: {
+                 "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
+             },
+             processing: true,
+             serverSide: true,
+             order: false,
+             ajax: {
+                 url: "/usuario/dinamizador/getDinamizador/" + nodo,
+                 type: "get",
+             },
+             columns: [
+             {
+                 data: 'tipodocumento',
+                 name: 'tipodocumento',
+             }, {
+                 data: 'documento',
+                 name: 'documento',
+             }, {
+                 data: 'nombre',
+                 name: 'nombre',
+             }, {
+                 data: 'email',
+                 name: 'email',
+             }, {
+                 data: 'telefono',
+                 name: 'telefono',
+             }, {
+                 data: 'estado',
+                 name: 'estado',
+             }, {
+                 data: 'detail',
+                 name: 'detail',
+                 orderable: false,
+             }, {
+                 data: 'edit',
+                 name: 'edit',
+                 orderable: false,
+             },
+             ],
+         });
+     }
+ }
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,56 +1,16 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-// mix.js('resources/js/app.js', 'public/js')
-//     .sass('resources/sass/app.scss', 'public/css');
 
 mix.js('resources/js/app.js', 'public/js');
-// mix.styles([
-// 		'resources/assets/css/libs/materialize/materialize.min.css',
-// 		'resources/assets/css/libs/metrojs/MetroJs.min.css',
-// 		'resources/assets/css/libs/weather-icons-master/weather-icons.min.css',
-// 		'resources/assets/css/libs/theme/alpha.css',
-// 		'resources/assets/css/libs/theme/custom.css'
-// 	],'public/css/libs.css');
-
-
-// mix.styles([
-// 		'resources/assets/plugins/materialize/css/materialize.css',
-// 		// 'resources/assets/plugins/materialize/css/material-icons.css',
-// 		'resources/assets/plugins/material-preloader/css/materialPreloader.min.css',
-// 		'resources/assets/plugins/weather-icons-master/css/weather-icons.min.css',
-// 		'resources/assets/plugins/datatables/css/jquery.dataTables.min.css',
-// 		'resources/assets/plugins/sweetalert/sweetalert.css',
-// 		'resources/assets/plugins/select2/css/select2.css',
-// 		'resources/assets/plugins/google-code-prettify/prettify.css',
-// 		'resources/assets/plugins/jquery-jvectormap/jquery-jvectormap-2.0.3.css',
-// 		'resources/assets/css/alpha.css',
-// 		// 'resources/assets/plugins/introjs/introjs.css',
-// 		'resources/assets/css/custom.css',
-// 		'resources/assets/plugins/bootstrap-datapicker/css/bootstrap-material-datetimepicker.css',
-// 		'resources/assets/plugins/materialize-clockpicker-master/dist/css/materialize.clockpicker.css',
-
-// 	],'public/css/libs.css');
-
 
 mix.styles([
 		'resources/assets/plugins/materialize/css/materialize.min.css',
 		'resources/assets/plugins/materialize/css/material-icons.css',
 		'resources/assets/plugins/datatables/css/jquery.dataTables.min.css',
-		'resources/assets/plugins/bootstrap-datapicker/css/bootstrap-material-datetimepicker.css',
 		'public/sweetalert2/sweetalert2.css',
-		'resources/assets/plugins/select2/css/select2.min.css',
+		'resources/assets/plugins/select2/css/select2.css',
 		'resources/assets/plugins/materialize-clockpicker-master/dist/css/materialize.clockpicker.css',
+		'resources/assets/plugins/bootstrap-datapicker/css/bootstrap-material-datetimepicker.css',
 		'resources/assets/plugins/dropzone/dropzone.css',
 		'resources/assets/plugins/dropzone/basic.min.css',
 		'resources/assets/css/alpha.css',
@@ -59,49 +19,6 @@ mix.styles([
 
 	],'public/css/libs.css');
 
-
-// mix.scripts([
-// 		'resources/assets/js/libs/jquery/jquery-2.2.0.min.js',
-// 		'resources/assets/js/libs/materialize/materialize.min.js',
-// 		'resources/assets/js/libs/materalize-preloader/materialPreloader.min.js',
-// 		'resources/assets/js/libs/jquery-blockui/jquery.blockui.js',
-// 		'resources/assets/js/libs/waypoints/jquery.waypoints.min.js',
-// 		'resources/assets/js/libs/counter-up-master/jquery.counterup.min.js',
-// 		'resources/assets/js/libs/jquery-sparkline/jquery.sparkline.min.js',
-// 		// 'resources/assets/js/libs/chartjs/chart.min.js',
-// 		'resources/assets/js/libs/flot/jquery.flot.min.js',
-// 		'resources/assets/js/libs/flot/jquery.flot.time.min.js',
-// 		'resources/assets/js/libs/flot/jquery.flot.symbol.min.js',
-// 		'resources/assets/js/libs/flot/jquery.flot.symbol.min.js',
-// 		'resources/assets/js/libs/flot/jquery.flot.tooltip.min.js',
-// 		'resources/assets/js/libs/curvedlines/curvedLines.js',
-// 		'resources/assets/js/libs/peity/jquery.peity.min.js',
-// 		'resources/assets/js/libs/theme/alpha.min.js',
-// 		// 'resources/assets/js/libs/theme/dashboard.js',
-// 	],'public/js/libs.js');
-//
-
-
-// mix.scripts([
-// 		'resources/assets/plugins/jquery/jquery-2.2.0.min.js',
-// 		'resources/assets/plugins/materialize/js/materialize.min.js',
-// 		'resources/assets/plugins/material-preloader/js/materialPreloader.min.js',
-// 		'resources/assets/plugins/jquery-blockui/jquery.blockui.js',
-// 		'resources/assets/plugins/google-code-prettify/prettify.js',
-// 		'resources/assets/plugins/datatables/js/jquery.dataTables.js',
-// 		'resources/assets/plugins/sweetalert/sweetalert.min.js',
-// 		'resources/assets/plugins/select2/js/select2.min.js',
-// 		'resources/assets/plugins/select2/js/i18n/es.js',
-// 		'resources/assets/plugins/datatables/js/pdfmake.min.js',
-// 		'resources/assets/plugins/datatables/js/vfs_fonts.js',
-// 		'resources/assets/plugins/datatables/js/bprint.min.js',
-// 		// 'resources/assets/js/pages/form-select2.js',
-// 		'resources/assets/plugins/materialize-clockpicker-master/dist/js/materialize.clockpicker.js',
-// 		'resources/assets/plugins/jquery-idletimer/idle-timer.js',
-// 		'resources/assets/js/alpha.min.js',
-
-// 	],'public/js/libs.js');
-//
 mix.scripts([
 		'resources/assets/plugins/jquery/dist/jquery.js',
 		'resources/assets/plugins/materialize/js/materialize.min.js',
@@ -109,12 +26,14 @@ mix.scripts([
 		'resources/assets/plugins/datatables/js/jquery.dataTables.js',
 		'public/sweetalert2/sweetalert2.min.js',
 		'resources/assets/plugins/select2/js/select2.min.js',
+		'resources/assets/plugins/select2/js/i18n/es.js',
 		'resources/assets/plugins/dropzone/dropzone.js',
 		'resources/assets/plugins/bootstrap-datapicker/js/bootstrap-material-datetimepicker.js',
 		'resources/assets/plugins/materialize-clockpicker-master/dist/js/materialize.clockpicker.js',
 		'resources/assets/js/alpha.js',
 		'resources/assets/js/custom.js',
 	],'public/js/libs.js');
+
 
 mix.scripts([
 		'resources/app/linea/administrador/index.js',
