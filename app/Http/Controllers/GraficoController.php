3c45d5af9ed06641fbf2c2b96d32042ad5151cb3
Victor Manuel Moreno Vega <vmmv99@hotmail.com>
2019-08-02 15:34:55 -0500

    Se logró hacer una gráfica usando highcharts con datos quemas desde del controlador

--- app/Http/Controllers/GraficoController.php
+++ app/Http/Controllers/GraficoController.php
@@ -38,6 +38,35 @@ class GraficoController extends Controller
     }
   }
 
+
+  /**
+  * @param array $datos
+  * @return array
+  */
+  private function agruparGestoresArray($datos) {
+    $gestor = array();
+    // $gestor = (object) $gestor;
+    // foreach ($datos as $key => $value) {
+    //   $gestor2 = $value->gestor;
+    //   array_push($gestor, $value->gestor);
+    // }
+
+
+    // $res2 = (object) $res2;
+    //
+    //
+    $gestor = array_column($datos->toArray(), 'gestor');
+    // $res2 = array();
+    foreach ($gestor as $key => $value) {
+      $res2[$value] = true;
+    }
+    $res2 = array_keys($res2);
+    // $res2 = array_diff_key($res2, $res2);
+
+
+    return $res2;
+  }
+
   /**
    *
    * @param int $id Id del nodo
@@ -47,11 +76,21 @@ class GraficoController extends Controller
   {
     if ( request()->ajax() ) {
       $datos = $this->articulacionRepository->tiposArticulacionesPorGestorYNodo($id);
-      $tipos_articulacion = array('Grupos de Investigación', 'Empresas', 'Emprendedores');
-      // $tipos_articulacion = ['Grupos de Investigación', 'Empresas', 'Emprendedores'];
+      $tipos_articulacion = array('gestores' => [0 => ['gestor' => 'Ramiro', 'grupos' => 5, 'empresas' => 8, 'emprendedores' => 5], 1 => ['gestor' => 'Julian', 'grupos' => 7, 'empresas' => 2, 'emprendedores' => 3]]);
+      $tipos2 = array('Grupos de Investigación', 'Empresas', 'Emprendedores');
+      // $tipos_articulacion = array_chunk($tipos_articulacion, true);
+      // $tipos_articulacion = array_values($tipos_articulacion);
+      $grupos = 0;
+      $empresas = 0;
+      $emprendedores = 0;
+      // dd($datos);
+      $gestor = $this->agruparGestoresArray($datos);
+      // return $gestor;
       return response()->json([
         'datos' => $datos,
-        'tipos_articulacion' => $tipos_articulacion
+        'controlador' => $gestor,
+        'tipos' => $tipos_articulacion,
+        'tipos2' => $tipos2
       ]);
     }
   }
--- resources/views/grafico/dinamizador/articulacion.blade.php
+++ resources/views/grafico/dinamizador/articulacion.blade.php
@@ -22,6 +22,7 @@
                     </div>
                   </div>
                   <div class="divider"></div>
+                  {{-- <a class="btn" onclick="exampleArticulaciones({{auth()->user()->dinamizador->nodo_id}})">Click me!</a> --}}
                   <div id="container" style="min-width: 310px; max-width: 800px; height: 400px; margin: 0 auto"></div>
                 </div>
               </div>
@@ -36,14 +37,140 @@
 @push('script')
   <script>
   $(document).ready(function(){
-    exampleArticulaciones({{auth()->user()->dinamizador->nodo_id}});
+    exampleArticulaciones({{auth()->user()->dinamizador->nodo_id}})
+    // Highcharts.chart('container', {
+    //   chart: {
+    //     type: 'column'
+    //   },
+    //   title: {
+    //     text: 'Articulaciones'
+    //   },
+    //   xAxis: {
+    //     categories: ['Grupos de Investigación', 'Empresas', 'Emprendedores'],
+    //     title: {
+    //       text: 'Tipos de Articulaciones'
+    //     }
+    //   },
+    //   yAxis: {
+    //     min: 0,
+    //     title: {
+    //       text: 'Número de Articulaciones'
+    //     }
+    //   },
+    //   legend: {
+    //     reversed: true
+    //   },
+    //   plotOptions: {
+    //     series: {
+    //       stacking: 'normal'
+    //     }
+    //   },
+    //   series:[
+    //     // {name: datos.gestores[1], data: [1, 2, 2]},
+    //     // {name: 'Empresas', data: [0, 0, 0]},
+    //     // {name: 'Emprendedores', data: [0, 0, 0]}
+    //   ],
+    // });
   });
+
+  function ciclo(array) {
+    $.each(array, function( index, value ) {
+      value;
+    });
+    // array.forEach(item => item)
+    // console.log(array);
+  }
+
   function exampleArticulaciones(id) {
     $.ajax({
       dataType: 'json',
       type: 'get',
       url: '/grafico/consultarArticulacionesPorNodo/'+id,
       success: function (data) {
+        var tamanho = data.tipos.gestores.length;
+        var datos = {
+          gestores: [],
+          articulaciones: [],
+          articulacionesb: [],
+          articulacionesc: []
+        };
+        // console.log(data.tipos);
+        for (var i = 0; i < tamanho; i++) {
+          if (data.tipos.gestores[i] != null) {
+            datos.gestores.push(data.tipos.gestores[i].gestor);
+            // console.log(data.tipos.gestores[i].gestor);
+          }
+        }
+
+        for (var i = 0; i < tamanho; i++) {
+          if (data.tipos.gestores[i].grupos != null) {
+            datos.articulaciones.push(data.tipos.gestores[i].grupos);
+            // console.log(data.tipos.gestores[i].a);
+          }
+        }
+
+        for (var i = 0; i < tamanho; i++) {
+          // if (data.tipos.gestores[i].empresas != null) {
+            datos.articulacionesb.push(data.tipos.gestores[i].empresas);
+            // console.log(data.tipos.gestores[i].a);
+          // }
+        }
+        for (var i = 0; i < tamanho; i++) {
+          if (data.tipos.gestores[i] != null) {
+            datos.articulacionesc.push(data.tipos.gestores[i].emprendedores);
+            // console.log(data.tipos.gestores[i].a);
+          }
+        }
+
+        console.log(datos.articulaciones);
+
+        var dataGraphic = [];
+
+        for (var i = 0; i < tamanho; i++) {
+          let array = '{"name": "'+datos.gestores[i]+'", "data": ['+datos.articulaciones[i]+', '+datos.articulacionesb[i]+', '+datos.articulacionesc[i]+']}';
+          // var part2 = '{"name": "datos", "data": [5, 5, 5]}';
+          array = JSON.parse(array);
+          dataGraphic.push(array);
+        }
+        // part2 = JSON.parse(part2);
+        // console.log();
+        // dataGraphic.push(part2);
+        // var part1 = true ? dataGraphic.push(JSON.parse("{name: 'datos.gestores', data: ['50', '10', '10']},")) : '';
+        // var part1 = true ? dataGraphic += "{name: 'datos.gestores'}}, {data: [ '50', '10', '10']}," : '';
+        // var parte2 = true ? dataGraphic.push(JSON.parse("{name: 'datos', data: ['1', '1', '1']},")) : '';
+
+        var resulta =  [{
+          name: 'John',
+          data: [5, 3, 4, 7, 2]
+        }, {
+          name: 'Jane',
+          data: [2, 2, 3, 2, 1]
+        }, {
+          name: 'Joe',
+          data: [3, 4, 4, 2, 5]
+        }]
+
+        // dataGraphic = Object.assign({}, dataGraphic);
+        // console.log();
+
+        // dataGraphic = [
+        //   dataGraphic
+        //   // {data: [
+        //   //   '50', '10', '10'
+        //   // ]},
+        // ]
+
+        // dataGraphic += [
+        //   {name: 'datos'},
+        //   {data: [
+        //     '50', '10', '10'
+        //   ]},
+        // ]
+
+        console.log(dataGraphic);
+        // console.log(resulta);
+
+        // console.log(datos.articulaciones);
         Highcharts.chart('container', {
           chart: {
             type: 'column'
@@ -52,9 +179,9 @@
             text: 'Articulaciones'
           },
           xAxis: {
-            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas'],
+            categories: ['Grupos de Investigación', 'Empresas', 'Emprendedores'],
             title: {
-              text: 'Gestores'
+              text: 'Tipos de Articulaciones'
             }
           },
           yAxis: {
@@ -71,16 +198,7 @@
               stacking: 'normal'
             }
           },
-          series: [{
-            name: 'John',
-            data: [5, 3, 4, 7, 2]
-          }, {
-            name: 'Jane',
-            data: [2, 2, 3, 2, 1]
-          }, {
-            name: 'Joe',
-            data: [3, 4, 4, 2, 5]
-          }]
+          series: dataGraphic
         });
       },
     });
