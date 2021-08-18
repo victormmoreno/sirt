8cb5527947dab3a795d7715d3b97b1031e027cba
jlondono433 <jlondono433@misena.edu.co>
2021-08-18 14:35:08 -0500

    reestructuracion de seeders para usos de infraestructura

--- database/seeds/AdviceAndUseOfPolymorphicInfrastructure.php
+++ database/seeds/AdviceAndUseOfPolymorphicInfrastructure.php
@@ -12,40 +12,43 @@ class AdviceAndUseOfPolymorphicInfrastructure extends Seeder
      */
     public function run()
     {
-
-        // $asesorias = UsoInfraestructura::with(['actividad.articulacion_proyecto.proyecto'])->whereHas('actividad.articulacion_proyecto.proyecto')->get();
         $asesorias = UsoInfraestructura::all();
 
         $asesorias->each(function ($item, $key)  {
             if($item->tipo_usoinfraestructura == 0 && isset($item->actividad->articulacion_proyecto->proyecto)){
-
-
                 $item->update([
                         'asesorable_id' => $item->actividad->articulacion_proyecto->proyecto->id,
                         'asesorable_type' => \App\Models\Proyecto::class
                 ]);
-
-
             }
             if($item->tipo_usoinfraestructura == 1 && isset($item->actividad->articulacionpbt)){
-
                 $item->update([
                         'asesorable_id' => $item->actividad->articulacionpbt->id,
                         'asesorable_type' => \App\Models\ArticulacionPbt::class
-                    ]);
+                ]);
             }
-            // dd($item->usogestores);
-            $item->usogestores->each(function ($item1, $key) use($item) {
-                if(isset($item1->user)){
-                    $user = App\User::where('id', $item1->user_id)->first();
+            if( isset($item->actividad->articulacion_proyecto->articulacion)){
+                $item->update([
+                    'asesorable_id' => $item->actividad->articulacion_proyecto->articulacion->id,
+                    'asesorable_type' => \App\Models\Articulacion::class
+                ]);
+            }
 
+            if( isset($item->actividad->edt)){
+                $item->update([
+                    'asesorable_id' => $item->actividad->edt->id,
+                    'asesorable_type' => \App\Models\Edt::class
+                ]);
+            }
+
+            $item->usogestores->each(function ($item1) use($item) {
+                if(isset($item1->user_id)){
+                    $user = App\User::withTrashed()->where('id', $item1->user_id)->first();
                     $item->usogestores()->update([
                         'asesorable_id' => $user->id,
                         'asesorable_type' => \App\User::class
                     ]);
                 }
-
-
             });
         });
     }
