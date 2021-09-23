565b047b0870fba6e987bfcb51ad464e74859a02
Victor Manuel Moreno Vera <vmmorenov@sena.edu.co>
2021-09-23 16:31:14 -0500

    Migraciones de evaluaciones

---
+++ database/migrations/2021_09_23_131402_create_evaluaciones_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEvaluacionesTable extends Migration
+{
+    public $tableName = 'evaluaciones';
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->unsignedInteger('evaluador_id');
+            $table->unsignedInteger('proyecto_id');
+            $table->dateTime('fecha_asignacion');
+            $table->dateTime('fecha_inicio')->nullable();
+            $table->dateTime('fecha_terminacion_cumplimiento')->nullable();
+            $table->dateTime('fecha_terminacion_calidad')->nullable();
+            $table->dateTime('fecha_terminado')->nullable();
+            $table->tinyInteger('aprobacion')->nullable();
+            $table->tinyInteger('tipo_evaluacion')->nullable();
+            $table->timestamps();
+
+
+            $table->foreign('evaluador_id')->references('id')->on('users');
+            $table->foreign('proyecto_id')->references('id')->on('proyectos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+}
