9f2396e8d0a6e70584555dbcdc332df13504d673
Dumuzid <vmmv99@hotmail.com>
2021-11-02 09:09:19 -0500

    Error con el modelo historial_entidad

--- app/Models/HistorialEntidad.php
+++ app/Models/HistorialEntidad.php
@@ -34,7 +34,7 @@ class HistorialEntidad extends Model
 
     public function user()
     {
-      return $this->belongsTo(User::class, 'user_id');
+      return $this->belongsTo(User::class, 'user_id')->withTrashed();
     }
 
     public function role()
