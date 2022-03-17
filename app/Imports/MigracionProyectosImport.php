b703db342307f2b8474bf973da77bcfd3c1a8929
Julian Dario Londono Raigosa <jdlondonor@sena.edu.co>
2022-03-17 13:35:07 -0500

    Se desarrollo el import de data para funcionarios y materiales

--- app/Imports/MigracionProyectosImport.php
+++ app/Imports/MigracionProyectosImport.php
@@ -20,7 +20,9 @@ class MigracionProyectosImport implements WithMultipleSheets
                 // 'Ideas' => new IdeasImport($this->nodo),
                 // 'Empresas' => new EmpresasImport(),
                 // 'Talentos' => new TalentosImport(),
-                'Funcionarios' => new FuncionarioImport($this->nodo)
+                'Funcionarios' => new FuncionarioImport($this->nodo),
+                'Materiales' => new MaterialImport($this->nodo),
+                'Equipos' => new EquipoImport($this->nodo),
                 // 'Grupos' => new GruposInvestigacionImport(),
                 // 'Gestores' => new GestoresImport($this->nodo),
                 // 'Proyectos' => new ProyectosImport($this->nodo)
